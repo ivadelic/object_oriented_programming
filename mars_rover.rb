caabf4bf7d4fa4c8b01d831b8717719a6f0288fb
Iva Delic <ms.iva.delic@gmail.com>
2015-04-03 13:19:54 -0400

    Adding string interpolation into Student and Instructor classes

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,4 +1,4 @@
-class Rover
+class Rover 
 	def initialize (init_x, init_y, init_direction)
 		@x = init_x
 		@y = init_y
@@ -6,18 +6,33 @@ class Rover
 	end
 
 	def read_instructions(user_input)
-		
+		commands = user_input.split(//)
+		commands.each do |x|
+			if x == "M"
+				move_forward
+			elsif x == "L"
+				turn_left
+			elsif x == "R"
+				turn_right
+			end
+		end
 	end
 
 	def move_forward 
 		if @direction == "N"
 			@y += 1
+		elsif @direction == "E"
+			@x += 1
+		elsif @direction == "S"
+			@y -= 1
+		else @direction == "W"
+			@x -=1
 		end
 	end
 
 	def turn_left
-		if @direction=="N"
-			@direction= "W"
+		if @direction == "N"
+			@direction = "W"
 		elsif @direction == "W"
 			@direction = "S"
 		elsif @direction == "S"
@@ -28,14 +43,14 @@ class Rover
 	end
 
 	def turn_right
-		if @direction=="N"
-			@direction="E"
-		elsif @direction=="E"
-			@direction="S"
-		elsif @direction=="S"
-			@direction="W"
-		else @direction=="W"
-			@direction="N"	
+		if @direction == "N"
+			@direction = "E"
+		elsif @direction == "E"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "W"
+		else @direction == "W"
+			@direction = "N"	
 		end
 			
 	end
@@ -45,7 +60,24 @@ class Rover
 	end
 end
 
-my_rover = Rover.new(2, 2, "N")
+my_rover = Rover.new(10,10, "N")
 puts my_rover
+
 my_rover.move_forward
 puts my_rover
+
+my_rover.read_instructions("MM")
+puts my_rover 
+
+# rover_2= Rover.new
+
+class Plateau 
+	def initialize(height, width)
+		@max_height = x
+		@max_width = y
+	end
+end
+
+the_plateau = Plateau.new(5,5)
+rover_with_restrictions= my_rover.the_plateau
+
--- people.rb
+++ people.rb
@@ -1,14 +1,25 @@
+class Person
+	def initialize(the_name)
+		@name = the_name
+	end
+end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object."
+	end
+end
+
 class Student < Person 
 	def learn
 		puts "I get it!"
 	end
 end
-class Instructor < Person
-	def teach
-		puts "Everything in Ruby is an Object."
-end
 
-class Person
-	def initialize(name)
-	end
-end
+instructor_name = Instructor.new(" ")
+puts "Hello my name is #{instructor_name}"
+
+student_name = Student.new(" ")
+puts "Hello my name is #{student_name }"
+
+
